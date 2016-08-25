ccc059dc79d3c43597af53c21e65b301071a8db6
garry <gsblackmore@gmail.com>
2016-08-25 16:43:48 +0100

    added the ability to use tap_dance and set tab to double as escape

--- keymap.c
+++ keymap.c
@@ -26,6 +26,17 @@ enum planck_keycodes {
   BACKLIT,
 };
 
+// Tap Dance declarations
+enum {
+	TBES = 0,
+};
+
+// Tap Dance definitions
+const qk_tap_dance_action_t tap_dance_actions[] = {
+	// once for backspace, twice for escape
+	[TBES] = ACTION_TAP_DANCE_DOUBLE(KC_TAB, KC_ESC),
+};
+
 // Fillers to make layering more clear
 #define _______ KC_TRNS
 #define XXXXXXX KC_NO
@@ -44,7 +55,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = {
-  {KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC},
+  {TD(TBES),KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC},
   {KC_QUOT, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT },
   {KC_LSPO, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSPC},
   {KC_LCTL, KC_LGUI, KC_LALT, KC_MEH, LOWER,   KC_SPC,  KC_SPC,  RAISE,    KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT}
